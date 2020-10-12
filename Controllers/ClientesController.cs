327367a9f9402beef30c110649d57d4fca8c01dd
TulioliAles <desmecad@hotmail.com>
2020-10-12 10:06:32 -0300

    Commit Web Api Asp.Net

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Estudo.WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Estudo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private static List<Clientes> clientes = new List<Clientes>();
+
+        public List<Clientes> Get()
+        {
+            return clientes;
+        }
+
+        public void Post(string nome)
+        {
+            if (!string.IsNullOrEmpty(nome))
+                clientes.Add(new Clientes(nome));
+        }
+
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estudo.WebApi.Models
+{
+    public class Clientes
+    {
+
+        public string Nome { get; set; }
+
+        public Clientes(string nome)
+        {
+            this.Nome = nome;
+        }
+
+    }
+}
